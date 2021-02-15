0a394737914ac89339910b682b48796d6e9cc0d6
Prasanta023 <76673869+Prasanta023@users.noreply.github.com>
2021-02-14 22:03:53 -0500

    pushing form pycharm to testbrn2

--- 01_hello/hello04_argparse_positional.py
+++ 01_hello/hello04_argparse_positional.py
@@ -3,7 +3,8 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Say hello')
-parser.add_argument('name', help='Name to greet')
+parser = argparse.ArgumentParser(description='This program Says Hello to the input name')
+parser.add_argument('name',help='Name to greet ')
+parser.add_argument('From',help='name of the place')
 args = parser.parse_args()
-print('Hello, ' + args.name + '!')
+print('Hello ' + args.name + ' from '+ args.From + '!')
