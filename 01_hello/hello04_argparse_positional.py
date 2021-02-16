e897638db447691c48d6f4ed1c8cd7f9523c50e7
Prasanta023 <76673869+Prasanta023@users.noreply.github.com>
2021-02-15 20:10:56 -0500

    use of argv and argparse

--- 01_hello/hello04_argparse_positional.py
+++ 01_hello/hello04_argparse_positional.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 # Purpose: Say hello
 
+import sys
 import argparse
 
-parser = argparse.ArgumentParser(description='This program Says Hello to the input name')
-parser.add_argument('name',help='Name to greet ')
-parser.add_argument('From',help='name of the place')
+# using argparse to read parameters
+parser = argparse.ArgumentParser(description='This Program accepts a file name and the path')
+parser.add_argument('name',help='FileName ',type=str)
+parser.add_argument('Path',help='Directory path ',type=str)
+# try:
 args = parser.parse_args()
-print('Hello ' + args.name + ' from '+ args.From + '!')
+print('File Name: ',args.name)
+print('File Path:',args.Path)
+
+# using the sys argv to read the input parameter
+print(len(sys.argv))
+print(sys.argv[0],sys.argv[1],sys.argv[2])
